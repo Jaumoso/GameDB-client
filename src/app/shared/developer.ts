6d2d901a66a1dab9052d1c28f390f924ec8be7af
Jaume Civera <jaumecvr2000@gmail.com>
2024-01-13 21:26:05 +0100

    Divided GameDB repository into 2 repositories, the GameDB-client and the GameDB-server

---
+++ src/app/shared/developer.ts
@@ -0,0 +1,6 @@
+export class Developer {
+    _id: string | undefined;
+    name: string | undefined;
+    description: string | undefined;
+    image: string | undefined;
+}
---
+++ src/app/shared/publisher.ts
@@ -0,0 +1,6 @@
+export class Publisher {
+    _id: string | undefined;
+    name: string | undefined;
+    description: string | undefined;
+    image: string | undefined;
+}
